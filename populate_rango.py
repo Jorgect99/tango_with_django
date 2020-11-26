f7c987aaa0f23c0b76d4e58da75df0162999797e
Jorge Cabral <jorgect99@gmail.com>
2020-11-25 22:51:36 -0700

    Add views and likes in Model Category

--- populate_rango.py
+++ populate_rango.py
@@ -8,6 +8,9 @@ from rango.models import Category, Page
 
 
 def populate():
+    Category.objects.all().delete()
+    Page.objects.all().delete()
+    
     python_pages = [
         {"title":"Oficial Python Tutorial","url":"http://dics.python.org/2/tutorial/"},
         {"title":"How to think like a computer scientist","url":"http://www.greenteapress.com/thinkpython"},
@@ -26,19 +29,19 @@ def populate():
     ]
 
     cats = {
-        "Python":{"pages":python_pages},
-        "Django":{"pages":django_pages},
-        "Other Frameworks":{"pages":other_pages},
+        "Python":{"pages":python_pages, "views":128, "likes":64},
+        "Django":{"pages":django_pages, "views":64, "likes":32},
+        "Other Frameworks":{"pages":other_pages, "views":32, "likes":16},
     }
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
             add_page(c,p["title"], p["url"])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category = c):
-            print("- {0} - {1}".format(str(c), p.algo()))
+            print("- {0} - {1}".format(str(c), str(p)))
  
 
 def add_page(cat, title, url, views=0):
@@ -49,8 +52,10 @@ def add_page(cat, title, url, views=0):
 
     return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.lies = likes
     c.save()
     return c
 
