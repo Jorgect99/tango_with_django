25cba265572b9adcef20809cf3b660775d10f2e3
Jorge Cabral <jorgect99@gmail.com>
2020-12-03 10:43:44 -0700

    Cookies and Sessionid

--- rango/views.py
+++ rango/views.py
@@ -3,18 +3,60 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm
+from datetime import datetime
+
 
 def index(request):
+    request.session.set_test_cookie()
+
     category_list = Category.objects.order_by('-likes')[:5]
     pages_list = Page.objects.order_by('-views')[:5]
    
     context_dict = {'categories': category_list, 'pages': pages_list}
 
-    return render(request, 'rango/index.html', context_dict)
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'rango/index.html', context_dict)
+
+    return response
+
+def get_server_side_cookie(request, cookie, default_value=None):
+    val = request.cookies.get(cookie)
+    if not val:
+        val = default_value
+    return val
+
+#cookie 
+def visitor_cookie_handler(request):
+    visits = int(request.COOKIES.get('visits','1'))
+
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_vist'] = last_visit_cookie
+        
+    request.session['visits'] = visits
+
+
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("TEST COOKIED WORKED!!")
+        request.session.delete_test_cookie()
+
+
     context_dict = {'my_name': "Jorge"}
-    return render(request, 'rango/about.html', context_dict)
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'rango/about.html', context_dict)
+    
+    return response
 
 
 def cat(request):
